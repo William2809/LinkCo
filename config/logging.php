649bc59d3aa358428cfaa3186d74527525d1fb93
William Huang <william.huang001@binus.ac.id>
2022-10-24 21:36:33 +0700

    last commit

--- config/logging.php
+++ config/logging.php
@@ -55,9 +55,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            'level' => 'debug',
         ],
 
         'daily' => [
@@ -82,7 +81,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
         ],
 
